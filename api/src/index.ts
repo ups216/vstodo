bacd70ae0976ff62c2aa9b41a0bf845146aef986
Lei Xu <leixu@leansoftx.com>
2023-01-29 11:57:23 +0800

    user entity and github auth working

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,76 @@
+import "reflect-metadata";
+require("dotenv-safe").config();
 import express from "express";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { User } from "./entities/User";
+import { join } from "path";
+import { Strategy as GitHubStrategy } from "passport-github";
+import passport from "passport";
+import jwt from "jsonwebtoken";
 
 const main = async () => {
+  await createConnection({
+    type: "postgres",
+    database: "vstodo",
+    dropSchema: true,
+    entities: [join(__dirname, "./entities/*.*")],
+    logging: !__prod__,
+    synchronize: !__prod__,
+  });
+
+  // const user = await User.create({ name: "John" }).save();
+  // console.log({ user });
+
   const app = express();
+
+  passport.serializeUser(function (user: any, done) {
+    done(null, user.accessToken);
+  });
+  app.use(passport.initialize());
+
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "http://localhost:3002/auth/github/callback",
+      },
+      async (_, __, profile, cb) => {
+        let user = await User.findOne({ where: { githubId: profile.id } });
+        if (user) {
+          user.name = profile.displayName;
+          await user.save();
+        } else {
+          user = await User.create({
+            name: profile.displayName,
+            githubId: profile.id,
+          });
+        }
+        cb(null, {
+          accessToken: jwt.sign({ userId: user.id }, "1121asdfasdf", {
+            expiresIn: "1y",
+          }),
+        });
+      }
+    )
+  );
+
+  app.get("/auth/github", passport.authenticate("github", { session: false }));
+
+  app.get(
+    "/auth/github/callback",
+    passport.authenticate("github", {
+      failureRedirect: "/login",
+      session: false,
+    }),
+    function (req: any, res) {
+      // Successful authentication, redirect home.
+      //res.send(req.user.accessToken);
+      res.redirect(`http://localhost:54321/auth/${req.user.accessToken}`);
+    }
+  );
+
   app.get("/", (_req, res) => {
     res.send("hello");
   });
